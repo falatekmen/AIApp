f774e5f068d8e9609f443a963f27ecb62c006641
ayseekmen <aysegucluekmen@gmail.com>
2022-12-08 20:16:51 +0300

    Add settings page

---
+++ src/pages/Settings.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+
+function Settings() {
+    return (
+        <SafeAreaView style={{backgroundColor: 'pink', flex: 1}}>
+            <Text>Hello Settings</Text>
+        </SafeAreaView>
+    );
+}
+
+export default Settings;
