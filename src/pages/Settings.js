f8d9096e3d463950c918a4878a54b7ac934b7944
ayseekmen <aysegucluekmen@gmail.com>
2022-12-09 00:36:33 +0300

    Update settings page

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { SafeAreaView, Text, TouchableOpacity } from "react-native";
 
+import LottieView from 'lottie-react-native'
+
 function Settings({navigation}) {
     return (
         <SafeAreaView style={{backgroundColor: 'black', flex: 1}}>
-            <TouchableOpacity style={{backgroundColor: 'red', width: 30, height: 30}}
+            <TouchableOpacity style={{backgroundColor: 'red', width: 60, height: 60}}
                     onPress={() => {
                         navigation.goBack()
                     }}></TouchableOpacity>
+                    <LottieView 
+                    source={require('../assets/animation/loading.json')} autoPlay loop />
         </SafeAreaView>
     );
 }
