4abb34b2b75d8c8b001c56861cdb40adaf2f4b2a
ayseekmen <aysegucluekmen@gmail.com>
2022-12-09 16:03:21 +0300

    Add mainNavigation page

---
+++ src/navigation/MainNavigation.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+import Main from '../pages/Main';
+import Settings from '../pages/Settings';
+
+const Stack = createNativeStackNavigator();
+
+function MainNavigation() {
+    return (
+    <NavigationContainer>
+        <Stack.Navigator
+         screenOptions={{
+           headerShown: false,
+         }}
+        >
+            <Stack.Screen name="MainPage" component={Main} />
+            <Stack.Screen name="SettingsPage" component={Settings} />
+        </Stack.Navigator>
+    </NavigationContainer>
+    );
+}
+
+export default MainNavigation;
