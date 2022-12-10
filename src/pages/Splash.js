f721235813d4df684ae4ec54f1b33f28f1a02af4
falatekmen <81239267+falatekmen@users.noreply.github.com>
2022-12-10 20:07:18 +0300

    Add splash

--- src/pages/Splash.js
+++ src/pages/Splash.js
@@ -1,9 +1,17 @@
 import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { setKey } from '../redux/KeyRedux'
+import { setLocalization } from '../redux/LocalizationRedux'
 
 const Splash = ({ navigation }) => {
 
+    const dispatch = useDispatch()
+
     setTimeout(() => {
+        dispatch(setLocalization("eng"))
+        dispatch(setKey(""))
+        dispatch(setModels)
         navigation.navigate("MainScreen")
     }, 500)
 
@@ -18,7 +26,7 @@ export default Splash
 
 const styles = StyleSheet.create({
     container: {
-        flex:1,
+        flex: 1,
         justifyContent: "center",
         alignItems: "center",
     }
