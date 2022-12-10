fa509e358805456ce9d1ac4dd5b795f3afcc4a80
falatekmen <81239267+falatekmen@users.noreply.github.com>
2022-12-10 14:39:22 +0300

    Update navigation

--- src/navigation/MainNavigation.js
+++ src/navigation/MainNavigation.js
@@ -2,23 +2,25 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
-import Main from '../pages/Main';
-import Settings from '../pages/Settings';
+import MainScreen from '../pages/MainScreen';
+import HowItsWork from '../pages/HowItsWork';
+import Splash from "../pages/Splash";
 
 const Stack = createNativeStackNavigator();
 
 function MainNavigation() {
     return (
-    <NavigationContainer>
-        <Stack.Navigator
-         screenOptions={{
-           headerShown: false,
-         }}
-        >
-            <Stack.Screen name="MainPage" component={Main} />
-            <Stack.Screen name="SettingsPage" component={Settings} />
-        </Stack.Navigator>
-    </NavigationContainer>
+        <NavigationContainer>
+            <Stack.Navigator
+                screenOptions={{
+                    headerShown: false,
+                }}
+            >
+                <Stack.Screen name="Splash" component={Splash} />
+                <Stack.Screen name="MainScreen" component={MainScreen} />
+                <Stack.Screen name="HowItsWork" component={HowItsWork} />
+            </Stack.Navigator>
+        </NavigationContainer>
     );
 }
 
