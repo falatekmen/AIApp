14e546c45d269e698b1f720e991df74b345e5cdf
falatekmen <81239267+falatekmen@users.noreply.github.com>
2022-12-11 17:36:28 +0300

    Downgrade rn version and change package name

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistor, store } from './redux/ReduxManager';
 import MainNavigation from './navigation/MainNavigation'
+import { LogBox } from "react-native";
+
+//XNOT: Bu hatalar çözüelcek
+
+const ignoreWarns = [
+  "Require cycle",
+];
+
+LogBox.ignoreLogs(ignoreWarns);
 
 function App() {
     return (
