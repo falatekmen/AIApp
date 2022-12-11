80ebeed682bbc987a44500af6ce7e6271f31e608
falatekmen <81239267+falatekmen@users.noreply.github.com>
2022-12-12 01:43:17 +0300

    Update redux init values

--- src/redux/ModelsDataRedux.js
+++ src/redux/ModelsDataRedux.js
@@ -4,8 +4,11 @@ const INITIAL_STATE = {
         name: "Ada",
         model: "text-ada-001",
         description: "Capable of very simple tasks, usually the fastest model in the GPT-3 series, and lowest cost.",
-        image: "https://user-images.githubusercontent.com/81239267/206866218-653e2419-214c-41b3-af2f-e85e288c0c77.png"
-    }],
+        image: "https://user-images.githubusercontent.com/81239267/206866218-653e2419-214c-41b3-af2f-e85e288c0c77.png",
+        tempeture: 0,
+        max_token: 64,
+        stop: null,
+    }]
 };
 
 // Selectors
