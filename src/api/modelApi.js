7d53c86d22299d3abb194cf14ffcaa7d2db93de3
falatekmen <81239267+falatekmen@users.noreply.github.com>
2022-12-12 01:41:54 +0300

    Update model api

--- src/api/modelApi.js
+++ src/api/modelApi.js
@@ -1,27 +1,28 @@
 import { Configuration, OpenAIApi } from "openai"
 
 
-export const getCompletion = async (text, key, temperature, model) => {
+export const getCompletion = async (text, key, temperature, model, max_token, stop) => {
 
     const configuration = new Configuration({
         apiKey: key,
     });
 
     const openai = new OpenAIApi(configuration);
-    
+
+    console.log({ text, key, temperature, model, max_token, stop })
     try {
         return await openai.createCompletion({
             // https://beta.openai.com/docs/quickstart/adjust-your-settings
             model: model,
             prompt: text,
-            // temelde modelin bu tahminleri yaparken ne kadar güvenli olması gerektiğini kontrol etmenizi sağlayan 
-            // 0 ile 1 arasında bir değerdir. Sıcaklığın düşürülmesi, daha az risk alacağı ve tamamlamaların daha doğru
+            // Sıcaklığın düşürülmesi, daha az risk alacağı ve tamamlamaların daha doğru
             // ve belirleyici olacağı anlamına gelir. Artan sıcaklık, daha çeşitli tamamlamalara neden olacaktır.
-            temperature: temperature,
-            max_tokens: 64,
+            temperature: temperature ? temperature : 0,
+            max_tokens: max_token ? max_token : 64,
             top_p: 1,
             frequency_penalty: 0,
             presence_penalty: 0,
+            stop: stop ? stop : null // eğer ["."] ise, verdiği cevapta ilk nokta işaretinde cevabı keser
         }).then((response) => {
             // console.log(response.data.choices[0].text.trim())
             return response.data.choices[0].text.trim()
