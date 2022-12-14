827578c782fabafac0a26902b582934370da6c25
falatekmen <81239267+falatekmen@users.noreply.github.com>
2022-12-14 23:55:01 +0300

    Update splash

--- src/pages/Splash.js
+++ src/pages/Splash.js
@@ -8,62 +8,19 @@ import { colors } from '../theme/Colors'
 import { units } from '../theme/Units'
 import { RemoteConfig } from '../firebase/RemoteConfig'
 import { setSelectedModel } from '../redux/SelectedModelRedux'
-import VersionCheck from "react-native-version-check"
+// import VersionCheck from "react-native-version-check"
+import mobileAds from 'react-native-google-mobile-ads';
+import { setAdFrequency } from '../redux/AdFrequencyRedux'
 
 
 const remoteConfig = new RemoteConfig()
-
 
 const Splash = ({ navigation }) => {
 
     const dispatch = useDispatch()
 
-
-    const data = [
-        {
-            "name": "Davinci",
-            "model": "text-davinci-003",
-            "description": "Most capable GPT-3 model. Can do any task the other models can do, often with higher quality, longer output and better instruction-following. Also supports inserting completions within text.",
-            "image": "https://user-images.githubusercontent.com/81239267/206864938-6388036c-93c5-4c96-9047-1bbbf32b46d8.png",
-            "max_token": 64,
-            "stop": null,
-            "temperature": 0,
-            "default": true
-        },
-        {
-            "name": "Curie",
-            "model": "text-curie-001",
-            "description": "Very capable, but faster and lower cost than Davinci.",
-            "image": "https://user-images.githubusercontent.com/81239267/206865339-ab7407b1-0d9d-44f0-856f-658709357262.png",
-            "max_token": 64,
-            "stop": null,
-            "temperature": 0,
-            "default": false
-        },
-        {
-            "name": "Babbage",
-            "model": "text-babbage-001",
-            "description": "Capable of straightforward tasks, very fast, and lower cost.",
-            "image": "https://user-images.githubusercontent.com/81239267/206865992-90cce3b4-6685-4273-8136-c9fe44de2e7e.png",
-            "max_token": 64,
-            "stop": null,
-            "temperature": 0,
-            "default": false
-        },
-        {
-            "name": "Ada",
-            "model": "text-ada-001",
-            "description": "Capable of very simple tasks, usually the fastest model in the GPT-3 series, and lowest cost.",
-            "image": "https://user-images.githubusercontent.com/81239267/206866218-653e2419-214c-41b3-af2f-e85e288c0c77.png",
-            "max_token": 64,
-            "stop": null,
-            "temperature": 0,
-            "default": false
-        }
-    ]
-
-
-// Freedashboard 
+    
+    // Freedashboard 
     // const appVersionCheck = () => {
     //     VersionCheck.needUpdate()
     //         .then(async res => {
@@ -79,24 +36,26 @@ const Splash = ({ navigation }) => {
         remoteConfig.init()
         // appVersionCheck()
 
+        mobileAds().initialize() // Initialize the Google Mobile Ads SDK
+
         const splash = () => {
+
             setTimeout(async () => {
-                //message to be shown in the alert
                 const key = await remoteConfig.getKey()
                 const models = await remoteConfig.getModels()
-                console.log(models)
-                if (models.length != 0) {
-                    console.log(["model atandı"])
+                const adFrequency = await remoteConfig.getAdFrequency()
 
+                if (models.length != 0) { // olaki hiç model gelmese, reduxtaki default modeli silmesin diyr
                     dispatch(setModelsData(models))
-
+                    // default=true olan modeli default model yapar
                     const defaultModel = models.find(e => e.default == true)
                     dispatch(setSelectedModel(defaultModel))
                 }
+                dispatch(setAdFrequency(adFrequency))
                 dispatch(setKey(key))
-
                 dispatch(setLocalization("eng")) // NOTEX: info ile teli yerini çek
                 navigation.navigate("MainScreen")
+
             }, 1000);
         }
 
