02fb0f7af636352f0553f8e69bc5583819dcf4db
falatekmen <81239267+falatekmen@users.noreply.github.com>
2022-12-21 23:43:43 +0300

    Update check update text

--- src/utils/CheckUpdate.js
+++ src/utils/CheckUpdate.js
@@ -3,13 +3,13 @@ import VersionCheck from "react-native-version-check";
 
 // test etmek için build alırken versiyon numarasını store'dakinden bir düşük yap(büyük yapınca çalışmaz)
 export default CheckUpdate = async () => {
-    
+
     await VersionCheck.needUpdate()
         .then((response) => {  // {"currentVersion": "1.0.1", "isNeeded": false, "latestVersion": "1.0.1", "storeUrl": "https://play.google.com/store/apps/details?id=com.ekmen.aiapp&hl=en&gl=US"}
             if (response && response.isNeeded) {
                 Alert.alert(
                     "Please Update",
-                    "You will have to update your app to the latest version to continue using it.",
+                    "You will have to update your app to the latest version to continue using it.\nIf you don't see the new app in the store, please wait a few minutes and try again later.",
                     [
                         {
                             text: "Update",
