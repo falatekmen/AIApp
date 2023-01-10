5d829e6cfcee7f88fec2ba570a2c6f3234ee3447
falatekmen <81239267+falatekmen@users.noreply.github.com>
2023-01-10 18:08:21 +0300

    Add noticicaiton handler when app at background state

--- index.js
+++ index.js
@@ -6,6 +6,15 @@
  import App from './src/App';
  import {name as appName} from './app.json';
  import 'react-native-url-polyfill/auto'
- 
+ import messaging from '@react-native-firebase/messaging';
+
+// Background & Quit state messages
+// To setup a background handler (for push notification), we called the
+// setBackgroundMessageHandler outside of your application logic 
+
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+    console.log('Message handled in the background!', remoteMessage);
+  });
+
  AppRegistry.registerComponent(appName, () => App);
  
